a55f4b34be84ed0394bcdeb1b002fcbdeb5ee3cc
Jetoff <johnflgn@gmail.com>
2016-02-19 23:34:45 -0500

    Commented SingleNumberIndex.java to include more information

--- SingleNumberIndex.java
+++ SingleNumberIndex.java
@@ -8,6 +8,12 @@ public class SingleNumberIndex {
 		
 	}
 	
+	/*
+	 * This code returns the index of a singleton in an array of pairs
+	 * Where the array is a sorted array of ints
+	 * It is similar to SingleNumber.java, and can be modified to return the number
+	 * Instead of just the index
+	 */
 	public int findSingleIndex(int[] myArray, int low, int high){
 		if(low == high){
 			System.out.println("Index is: " + low);
@@ -18,19 +24,31 @@ public class SingleNumberIndex {
 		System.out.println(low + " " + high + " " + midpoint);
 		
 		if(midpoint % 2 == 0){
+			//if the midpoint is an even index
+			//compare it to the number to it's right
 			if(myArray[midpoint] == myArray[midpoint + 1]){
+				//if they are the same
+				//take the right half
 				return 1 * findSingleIndex(myArray, midpoint + 2, high);
 			}
 			else{
+				//if they are different
+				//then take the left half
 				return 1 * findSingleIndex(myArray, low, midpoint);
 			}
 			
 		}
 		else{
+			//if the midpoint is an odd index
+			//compare it to the one on it's left
 			if(myArray[midpoint - 1] == myArray[midpoint]){
+				//if they are the same
+				//then take the right half
 				return 1 * findSingleIndex(myArray, midpoint + 1, high);
 			}
 			else{
+				//if they are different
+				//take the left half
 				return 1 * findSingleIndex(myArray, low, midpoint - 1);
 			}			
 		}		
